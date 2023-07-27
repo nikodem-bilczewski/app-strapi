29a88c2040e6c6cd0ea72caf08110a5142323770
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-07-27 10:32:11 +0200

    update config files

--- config/plugins.js
+++ config/plugins.js
@@ -7,16 +7,15 @@ module.exports = ({ env }) => ({
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
         params: {
+          ACL: env("AWS_ACL", "public-read"),
+          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
           Bucket: env("AWS_BUCKET_NAME"),
         },
       },
       actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
-        },
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
